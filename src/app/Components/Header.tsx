5c73ffa6f2140ab2f8561ce10b926066c96c77e9
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-09 13:57:50 +0100

    *edit* header menu

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -2,15 +2,17 @@ import Link from "next/link"
 
 function Header(){
     return(
-        <header className="flex flex-row justify-between items-center h-[80px] bg-amber-100 px-[20px]">
+        <header className="flex flex-row justify-between items-center h-[80px]">
             <div>YC Directory</div>
             <nav className="flex flex-row gap-x-[10px]">
-                <ul className="flex flex-row gap-x-[20px]">
+                <ul className="flex flex-row gap-x-[60px]">
                     <Link href="/">Home</Link>
                     <Link href="/page1">Page 1</Link>
                     <Link href="/page2">Page 2</Link>
+                    <Link href="/page2">Page 3</Link>
                 </ul>
             </nav>
+            <div>Login</div>
         </header>
     )
 }
