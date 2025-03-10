41bcbad51fce0d8c6aeb4ce00b3df77256104e30
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-10 13:24:06 +0100

    *edit* front / hero section improved design

--- src/app/Components/Hero/HeroSection.tsx
+++ src/app/Components/Hero/HeroSection.tsx
@@ -1,14 +1,19 @@
-// import Image from "next/image"
+import Image from "next/image"
 
 import SearchForm from "./SearchForm"
 
-function HeroSection(){
+function HeroSection({query} : {query? : string}){
     return(
-        <section className="flex flex-col w-full bg-gradient-to-r from-indigo-400 to-purple-300 h-[400px] pt-[30px] items-center">
-            <h1 className="text-6xl font-semibold text-white text-center leading-[80px]">Pitch your Startup<br/>to the Right Audience</h1>
-            <span className="text-white text-2xl mt-[40px] opacity-90">Connect with 1000s of Startup Founders and Investors in One Place</span>
-            {/*<Image src="/mega.png" alt="mega" width={200} height={200}/>*/}
-            <SearchForm/>
+        <section className="flex flex-row w-full justify-between bg-gradient-to-r from-indigo-200 to-fuchsia-100 h-[400px] pb-[10px] items-center px-[80px]">
+            <div className="flex flex-col ml-[50px]">
+                <h1 className="flex text-6xl font-bold items-center leading-[75px]">
+                    {/*<Image src="/coin.png" alt="coin" height={100} width={100} className="shrink-0 h-[130px] w-[130px] mr-[40px]"/>*/}
+                    Pitch your Startup<br/>to the Right Audience
+                </h1>
+                <span className="text-[22px] mt-[25px] leading-[30px]">Discover and invest in promising startups through our curated directory,<br/>connecting you with innovative entrepreneurs poised to transform industries.</span>
+                <SearchForm query={query}/>
+            </div>
+            <Image src="/rocket.png" alt="rocket" height={350} width={350}/>
         </section>
     )
 }
