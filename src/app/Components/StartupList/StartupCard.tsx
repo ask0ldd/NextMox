3e1ef28d546d7c8eb45b0e617c8f0cfceda971fd
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-11 12:07:51 +0100

    *edit* front / startup card & list style update

--- src/app/Components/StartupList/StartupCard.tsx
+++ src/app/Components/StartupList/StartupCard.tsx
@@ -3,19 +3,19 @@ import Image from "next/image"
 
 function StartupCard({startup} : {startup : IStartup}){
     return(
-        <article className="flex flex-col max-w-[calc(33.33333333%-20px/3)] w-[30%] bg-[#fafafa] shrink grow border-[#E5E5E5] border-1 p-[20px] rounded-[6px] hover:shadow-xl hover:bg-white cursor-pointer">
+        <article className="flex flex-col max-w-[calc(33.33333333%-20px/3)] w-[30%] bg-[#ffffff] shrink grow border-[#E3E3Ec] border-1 p-[20px] rounded-[6px] hover:shadow-xl hover:bg-white cursor-pointer">
             <div className="flex flex-row justify-between w-full">
                 <div className="flex flex-col">
                     <div className="flex flex-row gap-x-[20px] font-light">
-                        <span className="text-[20px] font-semibold translate-y-[-1px]">{startup.companyName}</span>
-                        <span className="text-[14px] mt-[4px]">{startup.location}</span>
+                        <span className="text-[20px] font-semibold translate-y-[-2px]">{startup.companyName}</span>
+                        <span className="text-[14px] mt-[4px] text-[#a5a5a5] font-normal">{startup.location}</span>
                     </div>
                     <div className="text-[16px] mt-[1px]">{startup.description.split('.')[0] + '.'}</div>
                 </div>
                 <Image className="grow-0 shrink-0 h-[45px] rounded-full" src={'/logos/' + startup.icon} width={45} height={45} alt={startup.companyName + "logo"}/>
             </div>
             <ul className="flex flex-row gap-x-[8px] text-[12px] mt-[14px]">
-                {startup.categories.slice(0, 4).map((category,i) => <li key={i} className="bg-neutral-100 border-1 border-gray-200 px-[10px] py-[5px] rounded-[3px]">{category.toUpperCase()}</li>)}
+                {startup.categories.slice(0, 4).map((category,i) => <li key={i} className="bg-[#f3f3f4] border-1 border-gray-200 px-[10px] py-[5px] rounded-[3px]">{category.toUpperCase()}</li>)}
             </ul>
             <Image 
                 src={'/landing/' + startup.pic} 
--- src/app/Components/StartupList/StartupList.tsx
+++ src/app/Components/StartupList/StartupList.tsx
@@ -7,8 +7,8 @@ async function StartupList({query} : {query? : string}){
     const startups = await startupService.getAll()
 
     return(
-        <section className="flex flex-col w-full">
-            <h2 className="text-[22px] font-semibold pt-[20px]">Recommended Startups</h2>
+        <section className="flex flex-col w-full shrink grow max-w-[1440px]">
+            <h2 className="text-[22px] font-semibold pt-[20px]">{!query ? 'Recommended Startups' : `Search Results for : "${query}"`}</h2>
             <div className="flex flex-row flex-wrap gap-x-[15px] gap-y-[15px] w-full mt-[20px]">
                 {startups.filter(startup => startup.companyName.toLowerCase().includes((query ?? "").toLowerCase())).map((startup, i) => 
                     <StartupCard key={"startup" + i} startup={startup}/>
