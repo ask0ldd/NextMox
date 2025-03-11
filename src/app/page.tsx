a4e88d5516416b61dc9cfda91067990f033094ce
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-11 12:07:08 +0100

    *edit* front / suspended list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+import { Suspense } from "react";
 import Header from "./Components/Header";
 import HeroSection from "./Components/Hero/HeroSection";
 import StartupList from "./Components/StartupList/StartupList";
+import SuspensedList from "./Components/StartupList/SuspensedList";
 
 export default async function Home({searchParams} : IProps) {
     const query = (await searchParams).query
 
     return (
-        <div className="flex flex-col min-h-screen max-w-[1440px] w-[100%] mx-auto pb-[40px]">
+        <div className="flex flex-col min-h-screen w-[100%] mx-auto pb-[40px] items-center">
             <Header/>
-            <main className="flex flex-col row-start-2 items-center sm:items-start">
+            <main className="flex flex-col row-start-2 items-center w-full justify-center">
                 <HeroSection query={query}/>
-                <StartupList query={query}/>
+                <Suspense fallback={<SuspensedList/>}>
+                    <StartupList query={query}/>
+                </Suspense>
             </main>
             <footer>
 
