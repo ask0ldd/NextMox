416d9df26cba9085c133abcc87b954b00b8ff43d
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-11 12:08:37 +0100

    *edit* front / header style update

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,15 +1,19 @@
 "use client"
+import { Rubik } from "next/font/google"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
+const anton = Rubik({
+    subsets: ["latin"],
+    weight: "700",
+})
 
 function Header(){
 
     const pathname = usePathname()
 
     return(
-        <header className="flex flex-row justify-between items-center h-[80px]">
-            <div className={`flex h-[40px] items-center`}>YC Directory</div>
+        <header className="flex flex-row max-w-[1440px] w-full shrink-0 grow-0 justify-between items-center h-[80px] relative">
             <nav className="flex flex-row gap-x-[10px]">
                 <ul className="flex flex-row gap-x-[60px] h-[40px]">
                     <Link href="/" className={pathname == "/" ? "border-b-[2px] border-[#373737] flex items-center" : "flex items-center"}>Home</Link>
@@ -18,6 +22,7 @@ function Header(){
                     <Link href="/page2" className="flex items-center">Page 3</Link>
                 </ul>
             </nav>
+            <div className={`${anton.className} flex justify-center h-[40px] w-[400px] items-center font-extrabold text-2xl absolute mx-[50%] translate-x-[-50%] text-center`}>YC Directory</div>
             <div className="flex justify-center items-center w-[140px] rounded-full text-white bg-[#373737] h-[40px] cursor-pointer px-[15px]">Login</div>
         </header>
     )
