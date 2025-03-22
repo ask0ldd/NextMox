348f2cfca96189d4fd82560777cc04674fdacb42
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-22 14:06:06 +0100

    *edit* front / hero section : fixing reset button

--- src/app/Components/Hero/ResetFormButton.tsx
+++ src/app/Components/Hero/ResetFormButton.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import Link from "next/link"
+
 function ResetFormButton(){
     // not using href since server side
     /*function resetForm(){
@@ -14,7 +16,9 @@ function ResetFormButton(){
     }
 
     return(
-        <input type="button" value={"Reset"} onClick={resetForm} className="w-[140px] text-white cursor-pointer bg-[#171717] px-[15px] h-[40px] rounded-full shadow-indigo-800 drop-shadow-md"/>
+        <button type="reset" onClick={resetForm} className="w-[140px] text-white cursor-pointer bg-[#171717] h-[40px] rounded-full shadow-indigo-800 drop-shadow-md">
+            <Link href="/" className="w-full h-full flex justify-center items-center px-[15px]">Reset</Link>
+        </button>
     )
 }
 
