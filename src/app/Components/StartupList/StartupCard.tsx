eaacddd23132798a39b686a5157b8043c340e038
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-22 14:06:40 +0100

    *edit* front / startup card : styling & transitions

--- src/app/Components/StartupList/StartupCard.tsx
+++ src/app/Components/StartupList/StartupCard.tsx
@@ -1,29 +1,38 @@
 import { IStartup } from "@/app/constants/startups"
 import Image from "next/image"
+import Link from "next/link"
 
 function StartupCard({startup} : {startup : IStartup}){
     return(
-        <article className="flex flex-col max-w-[calc(33.33333333%-20px/3)] w-[30%] bg-[#ffffff] shrink grow border-[#E3E3Ec] border-1 p-[20px] rounded-[6px] hover:shadow-xl hover:bg-white cursor-pointer">
-            <div className="flex flex-row justify-between w-full">
-                <div className="flex flex-col">
-                    <div className="flex flex-row gap-x-[20px] font-light">
-                        <span className="text-[20px] font-semibold translate-y-[-2px]">{startup.companyName}</span>
-                        <span className="text-[14px] mt-[4px] text-[#a5a5a5] font-normal">{startup.location}</span>
+        <Link className="group flex shrink grow max-w-[calc(33.33333333%-20px/3)] w-[30%]" href={`/startups/${startup.id}`}>
+            <article className="w-full bg-gradient-to-b from-[#ffffffee] to-[#ffffffbb] border-1 border-indigo-100 shrink grow p-[20px] rounded-[6px] hover:shadow-xl hover:bg-white cursor-pointer">
+                <div className="flex flex-row justify-between w-full">
+                    <div className="flex flex-col">
+                        <div className="flex flex-row gap-x-[20px] font-light">
+                            <span className="text-[20px] font-semibold translate-y-[-2px]">{startup.companyName}</span>
+                            <span className="text-[14px] mt-[4px] text-[#a5a5a5] font-normal">{startup.location}</span>
+                        </div>
+                        <div className="text-[16px] mt-[1px]">{startup.description.split('.')[0] + '.'}</div>
                     </div>
-                    <div className="text-[16px] mt-[1px]">{startup.description.split('.')[0] + '.'}</div>
+                    <Image className="grow-0 shrink-0 h-[45px] rounded-full" src={'/logos/' + startup.icon} width={45} height={45} alt={startup.companyName + "logo"}/>
                 </div>
-                <Image className="grow-0 shrink-0 h-[45px] rounded-full" src={'/logos/' + startup.icon} width={45} height={45} alt={startup.companyName + "logo"}/>
-            </div>
-            <ul className="flex flex-row gap-x-[8px] text-[12px] mt-[14px]">
-                {startup.categories.slice(0, 4).map((category,i) => <li key={i} className="bg-[#f3f3f4] border-1 border-gray-200 px-[10px] py-[5px] rounded-[3px]">{category.toUpperCase()}</li>)}
-            </ul>
-            <Image 
-                src={'/landing/' + startup.pic} 
-                width={1000} height={300} 
-                alt="screenshot"
-                className="w-full h-[300px] saturate-75 mt-[15px] object-cover object-center opacity-90"
-            />
-        </article>
+                <ul className="flex flex-row gap-x-[8px] text-[11px] mt-[14px]">
+                    {startup.categories.slice(0, 4).map((category,i) => 
+                            <li key={i} className="bg-[#291b5a25] border-1 border-[#291b5a30] px-[12px] py-[5px] rounded-full">
+                                {category.toUpperCase()}
+                            </li>
+                        )}
+                </ul>
+                <div className="flex w-full h-[300px] max-h-[300px] overflow-hidden rounded-[3px] border-1 border-[#a5a5a5] mt-[15px]">
+                    <Image 
+                        src={'/landing/' + startup.pic} 
+                        width={500} height={150} 
+                        alt="screenshot"
+                        className="w-full h-full saturate-75 object-cover object-center opacity-90 transition-transform duration-800 ease-in-out group-hover:scale-125 group-hover:saturate-90"
+                    />
+                </div>
+            </article>
+        </Link>
     )
 }
 
--- src/app/Components/StartupList/StartupList.tsx
+++ src/app/Components/StartupList/StartupList.tsx
@@ -1,18 +1,21 @@
 import StartupAPIService from "@/app/Services/StartupAPIService"
 import StartupCard from "./StartupCard"
+import { use } from "react"
 
-async function StartupList({query} : {query? : string}){
+function StartupList({query} : {query? : string}){
 
     const startupService = new StartupAPIService()
-    const startups = await startupService.getAll()
+    const startups = use(startupService.getAll())
 
     return(
-        <section className="flex flex-col w-full shrink grow max-w-[1440px]">
-            <h2 className="text-[22px] font-semibold pt-[20px]">{!query ? 'Recommended Startups' : `Search Results for : "${query}"`}</h2>
-            <div className="flex flex-row flex-wrap gap-x-[15px] gap-y-[15px] w-full mt-[20px]">
-                {startups.filter(startup => startup.companyName.toLowerCase().includes((query ?? "").toLowerCase())).map((startup, i) => 
-                    <StartupCard key={"startup" + i} startup={startup}/>
-                )}
+        <section className="flex flex-col w-full shrink grow items-center bg-gradient-to-t from-indigo-100 to-indigo-50 pb-[40px]">
+            <div className="flex flex-col w-full max-w-[1440px] shrink grow">
+                <h2 className="text-[22px] font-semibold pt-[20px]">{!query ? 'Recommended Startups' : `Search Results for : "${query}"`}</h2>
+                <div className="flex flex-row flex-wrap gap-x-[15px] gap-y-[15px] w-full mt-[20px]">
+                    {startups.filter(startup => startup.companyName.toLowerCase().includes((query ?? "").toLowerCase())).map((startup, i) => 
+                        <StartupCard key={"startup" + i} startup={startup}/>
+                    )}
+                </div>
             </div>
         </section>
     )
