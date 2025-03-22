6a33c3b201d47a97710a64ee7adf199fe8e55d41
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-22 14:05:36 +0100

    *edit* front / menu items defined

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -14,12 +14,12 @@ function Header(){
 
     return(
         <header className="flex flex-row max-w-[1440px] w-full shrink-0 grow-0 justify-between items-center h-[80px] relative">
-            <nav className="flex flex-row gap-x-[10px]">
-                <ul className="flex flex-row gap-x-[60px] h-[40px]">
-                    <Link href="/" className={pathname == "/" ? "border-b-[2px] border-[#373737] flex items-center" : "flex items-center"}>Home</Link>
-                    <Link href="/page1" className="flex items-center">Page 1</Link>
-                    <Link href="/page2" className="flex items-center">Page 2</Link>
-                    <Link href="/page2" className="flex items-center">Page 3</Link>
+            <nav className="flex flex-row gap-x-[10px] h-full items-center">
+                <ul className="flex flex-row gap-x-[10px] h-full">
+                    <Link href="/" className={pathname == "/" ? "border-y-[4px] border-indigo-300 flex items-center h-full px-[10px]" : "flex items-center h-full px-[10px]"}>Home</Link>
+                    <Link href="/page1" className="flex items-center h-full px-[10px]">About</Link>
+                    <Link href="/page2" className="flex items-center h-full px-[10px]">Startup Jobs</Link>
+                    <Link href="/page2" className="flex items-center h-full px-[10px]">Find a Co-founder</Link>
                 </ul>
             </nav>
             <div className={`${anton.className} flex justify-center h-[40px] w-[400px] items-center font-extrabold text-2xl absolute mx-[50%] translate-x-[-50%] text-center`}>YC Directory</div>
