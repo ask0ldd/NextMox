f243a1c4f5baadfda5311735ada5b6b8f7cf76d0
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-22 19:09:43 +0100

    *edit* front / startup cards : pill navigation

--- src/app/Components/StartupList/StartupCard.tsx
+++ src/app/Components/StartupList/StartupCard.tsx
@@ -1,11 +1,25 @@
+"use client"
 import { IStartup } from "@/app/constants/startups"
 import Image from "next/image"
 import Link from "next/link"
+import { redirect } from "next/navigation"
+import { MouseEvent } from "react"
 
 function StartupCard({startup} : {startup : IStartup}){
+
+
+    function handlePillClick(e : MouseEvent<HTMLLIElement>){
+        e.preventDefault()
+        e.stopPropagation()
+
+        const el = e.currentTarget as HTMLLIElement
+
+        redirect('/?query=' + el.textContent?.toLowerCase())
+    }
+
     return(
-        <Link className="group flex shrink grow max-w-[calc(33.33333333%-20px/3)] w-[30%]" href={`/startups/${startup.id}`}>
-            <article className="w-full bg-gradient-to-b from-[#ffffffee] to-[#ffffffbb] border-1 border-indigo-100 shrink grow p-[20px] rounded-[6px] hover:shadow-xl hover:bg-white cursor-pointer">
+        <Link className="group flex shrink grow max-w-[calc(33.33333333%-20px/3)] w-[30%] shadow-lg shadow-indigo-200 hover:shadow-[#a5b4fcaa] hover:shadow-[0_8px_16px_0px,0_4px_8px_0px] rounded-[6px] overflow-hidden" href={`/startups/${startup.id}`}>
+            <article className="w-full bg-gradient-to-b from-[#ffffffcc] to-[#ffffffaa] border-1 border-white shrink grow p-[20px] hover:bg-white cursor-pointer">
                 <div className="flex flex-row justify-between w-full">
                     <div className="flex flex-col">
                         <div className="flex flex-row gap-x-[20px] font-light">
@@ -14,21 +28,21 @@ function StartupCard({startup} : {startup : IStartup}){
                         </div>
                         <div className="text-[16px] mt-[1px]">{startup.description.split('.')[0] + '.'}</div>
                     </div>
-                    <Image className="grow-0 shrink-0 h-[45px] rounded-full" src={'/logos/' + startup.icon} width={45} height={45} alt={startup.companyName + "logo"}/>
+                    <Image className="grow-0 shrink-0 h-[45px] opacity-85 rounded-full" src={'/logos/' + startup.icon} width={45} height={45} alt={startup.companyName + "logo"}/>
                 </div>
                 <ul className="flex flex-row gap-x-[8px] text-[11px] mt-[14px]">
                     {startup.categories.slice(0, 4).map((category,i) => 
-                            <li key={i} className="bg-[#291b5a25] border-1 border-[#291b5a30] px-[12px] py-[5px] rounded-full">
+                            <li key={i} onClick={handlePillClick} className="bg-[#373737] text-white px-[12px] py-[6px] pt-[7px] rounded-[3px] hover:bg-[#291b5aaa]">
                                 {category.toUpperCase()}
                             </li>
                         )}
                 </ul>
-                <div className="flex w-full h-[300px] max-h-[300px] overflow-hidden rounded-[3px] border-1 border-[#a5a5a5] mt-[15px]">
+                <div className="flex w-full h-[300px] max-h-[300px] overflow-hidden rounded-[3px] border-1 border-[#373737] mt-[15px]">
                     <Image 
                         src={'/landing/' + startup.pic} 
                         width={500} height={150} 
                         alt="screenshot"
-                        className="w-full h-full saturate-75 object-cover object-center opacity-90 transition-transform duration-800 ease-in-out group-hover:scale-125 group-hover:saturate-90"
+                        className="w-full h-full saturate-65 object-cover object-center opacity-90 transition-transform duration-800 ease-in-out group-hover:scale-125 group-hover:saturate-85"
                     />
                 </div>
             </article>
@@ -37,3 +51,12 @@ function StartupCard({startup} : {startup : IStartup}){
 }
 
 export default StartupCard
+
+/*
+    Pre-Seed Stage
+    Seed Stage
+    Early Stage
+    Growth Stage
+    Expansion phase
+    Exit phase
+*/
