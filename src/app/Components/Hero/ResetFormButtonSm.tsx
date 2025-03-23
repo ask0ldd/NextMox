1aff44f220419bf82efcae118b8bdee466f41592
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-23 14:54:48 +0100

    *edit* front / search form : styling

--- src/app/Components/Hero/ResetFormButtonSm.tsx
+++ src/app/Components/Hero/ResetFormButtonSm.tsx
@@ -17,7 +17,9 @@ function ResetFormButtonSm(){
 
     return(
         <button type="reset" onClick={resetForm} className="bg-[#373737] text-white rounded-full shrink-0 w-[32px] h-[32px] mr-[3px]">
-            <Link href="/" className="w-full h-full flex justify-center items-center">x</Link>
+            <Link href="/" className="w-full h-full flex justify-center items-center">
+                <svg className="w-[18px] h-[18px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#FFFFFF" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+            </Link>
         </button>
     )
 }
--- src/app/Components/Hero/SearchForm.tsx
+++ src/app/Components/Hero/SearchForm.tsx
@@ -1,5 +1,4 @@
 import Form from "next/form"
-import ResetFormButton from "./ResetFormButton"
 import ResetFormButtonSm from "./ResetFormButtonSm"
 
 function SearchForm({query} : {query? : string}){
@@ -7,14 +6,14 @@ function SearchForm({query} : {query? : string}){
     return(
         <Form id="searchForm" action="/" className="flex w-full gap-x-[10px] mt-[40px]">
             {/* input name is mandatory since it will be used as parameter in the url query string */}
-            <div className="group w-full flex bg-white h-[40px] rounded-full pl-[15px] border-2 justify-center items-center">
+            <div className="w-full flex bg-white h-[40px] rounded-full pl-[13px] border-2 border-indigo-300 focus-within:outline-none focus-within:border-[#291b5aee] justify-center items-center">
                 <input 
                     id="searchInput"
                     name="query"
                     type="text"
                     defaultValue={query}
                     placeholder="Search for a Startup" 
-                    className="w-full focus:border-[#291b5aaa] focus:outline-none tracking-wider"
+                    className="w-full tracking-wider focus:border-none focus:outline-none"
                 />
                 {query && <ResetFormButtonSm/>}
             </div>
