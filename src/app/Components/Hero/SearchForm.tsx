0b31cd13943f02406083f7b7cf29dce8880a811d
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-24 15:52:03 +0100

    *edit* front : searchform : excludes transition logic

--- src/app/Components/Hero/SearchForm.tsx
+++ src/app/Components/Hero/SearchForm.tsx
@@ -1,10 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client"
 import Form from "next/form"
 import ResetFormButtonSm from "./ResetFormButtonSm"
+import SubmitButton from "./SubmitButton"
+import { Dispatch, FormEvent, SetStateAction } from "react"
+import { useRouter } from "next/navigation"
 
-function SearchForm({query} : {query? : string}){
+function SearchForm({query} : {query? : string, setIsPending? : Dispatch<SetStateAction<boolean>>}){
+
+    const router = useRouter()
+
+    function handleSearchSubmit(e: FormEvent<HTMLFormElement>){
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const queryValue = formData.get('query') as string
+        router.push(`/?query=${encodeURIComponent(queryValue)}`)
+    }
 
     return(
-        <Form id="searchForm" action="/" className="flex w-full gap-x-[10px] mt-[40px]">
+        <Form id="searchForm" action="/" onSubmit={handleSearchSubmit} className="flex w-full gap-x-[10px] mt-[40px]">
             {/* input name is mandatory since it will be used as parameter in the url query string */}
             <div className="w-full flex bg-white h-[40px] rounded-full pl-[13px] border-2 border-indigo-300 focus-within:outline-none focus-within:border-[#291b5aee] justify-center items-center">
                 <input 
@@ -13,16 +27,11 @@ function SearchForm({query} : {query? : string}){
                     type="text"
                     defaultValue={query}
                     placeholder="Search for a Startup" 
-                    className="w-full tracking-wider focus:border-none focus:outline-none"
+                    className="w-full tracking-wide focus:border-none focus:outline-none"
                 />
                 {query && <ResetFormButtonSm/>}
             </div>
-            <button 
-                type="submit" 
-                className="w-[180px] text-white cursor-pointer bg-[#373737] px-[15px] h-[40px] rounded-full shadow-indigo-800 drop-shadow-md hover:bg-[#291b5aaa]"
-            >
-                Search
-            </button>
+            <SubmitButton/>
         </Form>
     )
 }
