e0d30b704fe355308e5d6b5cfa34955dfb34c84f
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-25 18:48:40 +0100

    *edit* front / startup page : first section added

--- src/app/startups/[id]/page.tsx
+++ src/app/startups/[id]/page.tsx
@@ -1,5 +1,7 @@
 import Header from "@/app/Components/Header";
+import CardPill from "@/app/Components/StartupList/CardPill";
 import StartupAPIService from "@/app/Services/StartupAPIService";
+import Image from "next/image"
 
 // params prop is passed by default by next
 export default async function StartupPage({params} : IProps) {
@@ -9,17 +11,37 @@ export default async function StartupPage({params} : IProps) {
     const startup = await startupService.getById(parseInt(id))
 
   return (
-    <div className="flex flex-col min-h-screen w-[100%] mx-auto pb-[40px] items-center">
-      <Header/>
-      <main className="flex flex-col row-start-2 items-center w-full justify-center">
-        <div className="flex flex-col w-full max-w-[1440px]">
-          <span>{`Home > Companies > ${startup?.companyName}`}</span>
+        <div className="flex flex-col min-h-screen w-[100%] mx-auto pb-[40px] items-center">
+            <Header/>
+            <main className="flex flex-col row-start-2 items-center w-full justify-center">
+                <div className="flex flex-col w-full max-w-[1440px]">
+                    <span>{`Home > Companies > ${startup?.companyName}`}</span>
+                    <section className="flex w-full gap-x-[30px] mt-[20px]">
+                        <div className="flex grow shrink flex-col w-full">
+                            <div className="flex flex-row gap-x-[20px]">
+                                <Image className="grow-0 shrink-0 opacity-85 rounded-[6px]" src={'/logos/' + startup?.icon} width={128} height={128} alt={startup?.companyName + "logo"}/>
+                                <div className="flex flex-col">
+                                    <span className="text-[30px]">{startup?.companyName}</span>
+                                    <span>{startup?.description.split('.')[0] + '.'}</span>
+                                    <ul className="flex flex-row gap-x-[5px]">
+                                        {startup?.categories.slice(0, 4).map((category, idx) => <CardPill key={'pill' + idx} category={category}/>)}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col grow-0 shrink-0 w-[300px] bg-[#efefef] p-[20px] rounded-[6px]">
+                            <span>{startup?.companyName}</span>
+                            <div className="flex justify-between"><span>Founded:</span><span>{startup?.founded}</span></div>
+                            <div className="flex justify-between"><span>Team Size:</span><span>{startup?.teamSize}</span></div>
+                            <div className="flex justify-between"><span>Status:</span><span>{startup?.status}</span></div>
+                            <div className="flex justify-between"><span>Location:</span><span>{startup?.location.split(',')[0]}</span></div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+            <footer>
+            </footer>
         </div>
-      </main>
-      <footer>
-        {id}
-      </footer>
-  </div>
   );
 }
 
