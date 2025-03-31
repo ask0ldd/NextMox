7fef9a188cd2da6c5c3f39b74a2d1448fa9f0939
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-31 14:44:51 +0200

    *edit* front / header : adding login management logic

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -2,6 +2,12 @@
 import { Rubik } from "next/font/google"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { authAPIService } from "../Services/AuthAPIService"
+import { localStorageService } from "../Services/LocalStorageService"
+import { jwtDecode } from "jwt-decode"
+// import { useEffect, useState } from "react"
+import IJWTToken from "../interfaces/IJWTToken"
+import useLocalStorage from "../hooks/useLocalStorage"
 
 const anton = Rubik({
     subsets: ["latin"],
@@ -11,6 +17,36 @@ const anton = Rubik({
 function Header(){
 
     const pathname = usePathname()
+
+    const token = useLocalStorage()
+
+    // const [token, setToken] = useState<string | null>(null)
+
+    /*useEffect(() => {
+        const tk = localStorageService.getToken() // !!! useSyncExternalStore
+        setToken(tk)
+    }, [])*/
+
+    async function login(){
+        if(token) {
+            logOut()
+            return
+        }
+        const tk = await authAPIService.login({username : "maria", password : "guess"})
+        localStorageService.setToken(tk)
+        // setToken(tk)
+    }
+
+    function logOut(){
+        localStorageService.empty()
+        // setToken(null)
+    }
+
+    function getName() : string | null {
+        if (!token) return null
+        const { username } = jwtDecode(token) as IJWTToken
+        return username || null
+    }
 
     return(
         <header className="flex flex-row max-w-[1440px] w-full shrink-0 grow-0 justify-between items-center h-[80px] relative">
@@ -23,7 +59,13 @@ function Header(){
                 </ul>
             </nav>
             <div className={`${anton.className} flex justify-center h-[40px] w-[400px] items-center font-extrabold text-2xl absolute mx-[50%] translate-x-[-50%] text-center`}>YC Directory</div>
-            <div className="flex justify-center items-center w-[140px] rounded-full text-white bg-[#373737] h-[40px] cursor-pointer px-[15px]">Login</div>
+            <div 
+                onClick={login} 
+                role="button" 
+                aria-label={!getName() ? 'Login' : `Logged in as ${getName()}`} 
+                className="flex justify-center items-center w-[140px] rounded-full text-white bg-[#373737] h-[40px] cursor-pointer px-[15px]">
+                    {!getName() ? 'Login' : 'Hi ' + getName()}
+                </div>
         </header>
     )
 }
